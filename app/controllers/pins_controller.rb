ba99e9e0a2d6a8dbf06fda984e79d58d3d00b3da
Richard Montenegro <rjent767@gmail.com>
2015-05-05 15:04:55 -0700

    added 70 pins per page

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 30)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 70)
   end
 
   def show
