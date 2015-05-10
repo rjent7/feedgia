4570ca6af641666915f03af8a10ed80eab15d7bd
Richard Montenegro <rjent767@gmail.com>
2015-05-09 21:56:14 -0700

    changed some designs

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about"
+  
   resources :pins do
-      member do
-        put "like" => "pins#upvote"
-      end
+    member do
+      put "like" => "pins#upvote"
     end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
