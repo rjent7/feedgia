a597acd0e20f248d26227243d7fc600b6f3a0f10
Richard Montenegro <rjent767@gmail.com>
2015-05-15 18:03:36 -0700

    added funny notifications

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -20,7 +20,7 @@ class PinsController < ApplicationController
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
+      redirect_to @pin, notice: 'You a bitch :)'
     else
       render action: 'new'
     end
